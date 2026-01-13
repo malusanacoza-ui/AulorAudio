2bfd0399169799070e302b5c1f63c2a89e8b38e0
malusanacoza-ui <malusana.co.za@gmail.com>
2026-01-13 09:22:27 +0200

    Add project files.

---
+++ Data/Migrations/20260113071851_AddSongs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AulorAudio.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSongs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CoverImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,12 @@
+﻿public class Song
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+    public string Artist { get; set; }
+
+    public string FilePath { get; set; }   // where MP3 is stored
+    public string CoverImage { get; set; } // optional album cover
+
+    public DateTime UploadDate { get; set; } = DateTime.Now;
+}
